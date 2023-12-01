58c43abd3c8288525dc18c9539d46cf7e6294956
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2023-12-01 17:04:22 +0530

    add character data fetching logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,69 @@
+import { useEffect, useState } from "react";
+
+interface Character {
+    id: number;
+    firstName: string;
+    lastName: string;
+    fullName: string;
+    title: string;
+    family: string;
+    image: string;
+    imageUrl: string;
+}
+
 function App() {
-    return <h1>Hello World</h1>;
+    const [characters, setCharacters] = useState<Character[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchCharacters = async () => {
+            try {
+                const response = await fetch(
+                    "https://thronesapi.com/api/v2/Characters"
+                );
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data");
+                }
+
+                const data: Character[] = await response.json();
+                setCharacters(data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                setLoading(false);
+            }
+        };
+
+        fetchCharacters();
+    });
+
+    return (
+        <div>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    <ul>
+                        {characters.map((character) => (
+                            <li>
+                                <div>
+                                    <img
+                                        src={character.imageUrl}
+                                        alt={character.fullName}
+                                    />
+                                </div>
+                                <div>
+                                    <p>ID: {character.id}</p>
+                                    <p>Name: {character.fullName}</p>
+                                    <p>Family: {character.family}</p>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default App;
